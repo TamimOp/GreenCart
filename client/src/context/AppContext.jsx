4e32a0fb3a68d419bbcebaf992f283322a7a8840
TamimOp <tamimshadanik@gmail.com>
2025-04-23 19:11:21 +0600

    best seller single product added with add remove notification functionallity

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
+import { toast } from "react-hot-toast";
 
 export const AppContext = createContext();
 
@@ -12,9 +13,44 @@ export const AppContextProvider = ({ children }) => {
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState({});
 
+  //Fetching products
   const fetchProducts = async () => {
     setProducts(dummyProducts);
+  };
+
+  //adding products to cart
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+    setCartItems(cartData);
+    toast.success("Item added to cart");
+  };
+
+  //Updating cart items
+  const updateCartItems = (itemId, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData);
+    toast.success("Cart updated");
+  };
+
+  //removing items from cart
+  const removeFromCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] -= 1;
+      if (cartData[itemId] === 0) {
+        delete cartData[itemId];
+      }
+    }
+    toast.success("Item removed from cart");
+    setCartItems(cartData);
   };
 
   useEffect(() => {
@@ -31,6 +67,10 @@ export const AppContextProvider = ({ children }) => {
     setShowUserLogin,
     products,
     currency,
+    addToCart,
+    updateCartItems,
+    removeFromCart,
+    cartItems,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
